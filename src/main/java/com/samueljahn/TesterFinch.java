0bd9fa2518f89caf040b0b99a28ee5903de193d9
THEGREATESTSamuel <sckovler@gmail.com>
2026-06-01 12:25:27 -0700

    Update TesterFinch.java

--- src/main/java/com/samueljahn/TesterFinch.java
+++ src/main/java/com/samueljahn/TesterFinch.java
@@ -1,5 +1,12 @@
-package samueljahn
+package samueljahn;
+
+public static void main(String[] args) {
+
 
 public class TesterFinch {
+
     FinalFinch bird =  new FinalFinch(device:"a")
+    bird.setMove("F", 50, 50);
+}
+
 }
